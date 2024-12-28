ff02ed3e8f578a9bd94a35d8e348916e48716892
Skeptic <github@jonas-vieth.de>
2024-12-28 18:47:07 +0100

    reading of docker env

--- RFE/Program.cs
+++ RFE/Program.cs
@@ -3,20 +3,27 @@ using RFE.Components.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Bind environment variables to configuration
+builder.Configuration.AddEnvironmentVariables();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
+// Add services to the container
 builder.Services.AddSingleton<ISettingsService, SettingsService>();
 builder.Services.AddSingleton<IFileService, FileService>();
 
-// Add services to the container.
+
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddHttpClient();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -25,11 +32,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Map Razor Components
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
